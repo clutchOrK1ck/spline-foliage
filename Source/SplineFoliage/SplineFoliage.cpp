c767c445bb29154051000f43e5131ebd9ae3a4e5
clutchOrKick <serhii.poklonskyi@gmail.com>
2026-03-27 14:21:11 +0100

    initial project commit

---
+++ Source/SplineFoliage/SplineFoliage.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SplineFoliage.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SplineFoliage, "SplineFoliage" );
